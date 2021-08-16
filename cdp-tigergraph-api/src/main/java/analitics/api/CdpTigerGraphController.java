0269b4106c134e53967e83266a34629442ef463c
Antonio Casado <antonio.casado@ippen-digital.de>
2021-08-16 11:55:52 +0200

    code

---
+++ cdp-tigergraph-api/src/main/java/analitics/api/CdpTigerGraphController.java
@@ -0,0 +1,18 @@
+package analitics.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class CdpTigerGraphController {
+
+
+    @GetMapping("/{userData}")
+    public String getUserData() {
+        return null;
+    }
+}
---
+++ cdp-tigergraph-api/src/main/java/analitics/client/CdpHttpAsyncClient.java
@@ -0,0 +1,67 @@
+package analitics.client;
+
+import de.cdp.tg.analitics.config.ApplicationConfig;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+
+@Component
+@RequiredArgsConstructor
+public class CdpHttpAsyncClient {
+
+    private final ApplicationConfig config;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CdpHttpAsyncClient.class);
+
+
+    public List<String> getCdp(String userId) throws InterruptedException, ExecutionException, TimeoutException {
+
+        LOGGER.trace("Retrieve data Tigergraph");
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create(getUrlBuilder(userId)))
+                .setHeader("Content-Type", "application/json")
+                .build();
+
+        HttpClient httpAsyncClient = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_1_1)
+                .connectTimeout(Duration.ofSeconds(1))
+                .build();
+
+
+        // sendAsync(): Sends the given request asynchronously using this client with the given response body handler.
+        CompletableFuture<HttpResponse<String>> asyncResponse = httpAsyncClient.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+
+        String asyncResultBody = asyncResponse.thenApply(HttpResponse::body).get(config.getGraphDbConnectTimeoutMs(), TimeUnit.MILLISECONDS);
+        LOGGER.trace("Connection Succesful");
+
+        return getFormattedStringList(asyncResultBody);
+    }
+
+    private List<String> getFormattedStringList(String body) {
+        String substring = body.substring(1, body.length() - 1);
+        return Arrays.asList(substring.split(",", -1));
+    }
+
+    private String getUrlBuilder(String userId) {
+
+        return new StringBuilder()
+                .append(config.getGraphDbBaseUrl())
+                .append(String.format("?userId=%s", userId))
+                .toString();
+    }
+}
